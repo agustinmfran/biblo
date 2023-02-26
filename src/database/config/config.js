08508e8c89a0eb54e4bc2849b93e312c3eed809d
agustinmfran <agustinmfran@gmail.com>
2023-02-26 13:53:05 +0100

    pruebas 13:53

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -12,16 +12,16 @@ module.exports = {
   },
   test: {
     username: "root",
-    password: "flixar",
-    database: "database_test",
-    host: "127.0.0.1",
+    password: "supernova22",
+    database: "biblo_db",
+    host: "34.163.134.235",
     dialect: "mysql",
   },
   production: {
     username: "root",
-    password: "root",
-    database: "database_production",
-    host: "127.0.0.1",
+    password: "supernova22",
+    database: "biblo_db",
+    host: "34.163.134.235",
     dialect: "mysql",
   },
 };
